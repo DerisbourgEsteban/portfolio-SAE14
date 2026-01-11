de9905f35f8b20d441fbbe7351995e6e69633a48
DerisbourgEsteban <estebanderisbourg@gmail.com>
2026-01-11 15:15:57 +0100

    modifs

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,20 +90,20 @@
   window.addEventListener('load', aosInit);
 
   /**
-   * Init typed.js
-   */
-  const selectTyped = document.querySelector('.typed');
-  if (selectTyped) {
-    let typed_strings = selectTyped.getAttribute('data-typed-items');
-    typed_strings = typed_strings.split(',');
-    new Typed('.typed', {
-      strings: typed_strings,
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 50,
-      backDelay: 2000
-    });
-  }
+ * Init typed.js
+ */
+const selectTyped = document.querySelector('.typed');
+if (selectTyped) {
+  let typed_strings = selectTyped.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 80,  // <--- AUGMENTER ici pour écrire plus lentement (ex: 150 au lieu de 100)
+    backSpeed: 50,   // Vitesse de suppression des lettres
+    backDelay: 2500  // <--- AUGMENTER ici pour que le texte reste affiché plus longtemps (ex: 4000ms = 4 sec)
+  });
+}
 
   /**
    * Initiate Pure Counter
